e2515f82e3634d5ce918db207d0dcf358c574eb5
philip-loeffler <philiploeffler@gmail.com>
2021-10-28 11:03:57 -0400

    fixed up design

--- src/Components/SystemInventoryGrid.js
+++ src/Components/SystemInventoryGrid.js
@@ -7,7 +7,7 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import Paper from '@material-ui/core/Paper';
 import Chip from '@material-ui/core/Chip';
-
+import Pagination from '@material-ui/lab/Pagination';
 import Search from '@material-ui/icons/Search';
 import FileCopyRounded from '@material-ui/icons/FileCopyRounded';
 import DeleteRounded from '@material-ui/icons/DeleteRounded';
@@ -55,60 +55,73 @@ export default function SystemInventoryGrid(props) {
   };
 
   return (
-    <TableContainer
-      className={classes.tableContainer}
-      component={Paper}
-      elevation={4}
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
     >
-      <div className={classes.searchPadding}>
-        <SearchBar
-          variant="outlined"
-          placeholder="Search resources"
-          InputProps={{
-            startAdornment: <Search />,
-          }}
-        />
+      <div
+        style={{
+          width: '80%',
+        }}
+      >
+        <TableContainer component={Paper} elevation={4}>
+          <div className={classes.searchPadding}>
+            <SearchBar
+              variant="outlined"
+              placeholder="Search resources"
+              InputProps={{
+                startAdornment: <Search />,
+              }}
+            />
+          </div>
+
+          <Table sx={{ minWidth: 700 }} aria-label="customized table">
+            <TableHead>
+              {rowTitle.map((title, i) => (
+                <StyledTableCell>{title}</StyledTableCell>
+              ))}
+            </TableHead>
+            <TableBody>
+              {systemInventoryData.map((data, i) => (
+                <StyledTableRow stripedRows key={i}>
+                  <StyledTableCell className={classes.anthemBlue}>
+                    {data.name}
+                  </StyledTableCell>
+                  <StyledTableCell>{data.businessOwner}</StyledTableCell>
+
+                  <StyledTableCell>{data.technicalOwner}</StyledTableCell>
+                  <StyledTableCell>
+                    <Chip
+                      color="success"
+                      label={data.risk}
+                      className={handleChipColor(data.risk)}
+                    ></Chip>
+                  </StyledTableCell>
+                  <StyledTableCell>
+                    {data.lodOne}
+                    <CaptionTypography>{data.technicalOwner}</CaptionTypography>
+                  </StyledTableCell>
+                  <StyledTableCell>
+                    {data.lodTwo}
+                    <CaptionTypography>{data.businessOwner}</CaptionTypography>
+                  </StyledTableCell>
+                  <StyledTableCell>
+                    {data.lodThree}
+                    <CaptionTypography>{data.technicalOwner}</CaptionTypography>
+                  </StyledTableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <Box mt={10} />
+        <div style={{ display: 'flex', justifyContent: 'end' }}>
+          <Pagination count={10} />
+        </div>
       </div>
-
-      <Table sx={{ minWidth: 700 }} aria-label="customized table">
-        <TableHead>
-          {rowTitle.map((title, i) => (
-            <StyledTableCell>{title}</StyledTableCell>
-          ))}
-        </TableHead>
-        <TableBody>
-          {systemInventoryData.map((data, i) => (
-            <StyledTableRow stripedRows key={i}>
-              <StyledTableCell className={classes.anthemBlue}>
-                {data.name}
-              </StyledTableCell>
-              <StyledTableCell>{data.businessOwner}</StyledTableCell>
-
-              <StyledTableCell>{data.technicalOwner}</StyledTableCell>
-              <StyledTableCell>
-                <Chip
-                  color="success"
-                  label={data.risk}
-                  className={handleChipColor(data.risk)}
-                ></Chip>
-              </StyledTableCell>
-              <StyledTableCell>
-                {data.lodOne}
-                <CaptionTypography>{data.technicalOwner}</CaptionTypography>
-              </StyledTableCell>
-              <StyledTableCell>
-                {data.lodTwo}
-                <CaptionTypography>{data.businessOwner}</CaptionTypography>
-              </StyledTableCell>
-              <StyledTableCell>
-                {data.lodThree}
-                <CaptionTypography>{data.technicalOwner}</CaptionTypography>
-              </StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-      {/* <TablePagination count={10} /> */}
-    </TableContainer>
+    </div>
   );
 }
--- src/views/SystemInventory.js
+++ src/views/SystemInventory.js
@@ -9,51 +9,46 @@ export default function SystemInventory(props) {
   const backgroundImage = (img.src = '../img/landing-background.png');
 
   // uncomment out this when landing page is merged
-  // function SystemInventoryBackground() {
-  //   return (
-  //     <div
-  //       style={{
-  //         backgroundImage: `url(${backgroundImage})`,
-  //         width: '99.6vw',
-  //         height: '35vh',
-  //         position: 'relative',
-  //         left: '50%',
-  //         right: '50%',
-  //         top: '-100px',
-  //         marginLeft: '-50vw',
-  //         marginRight: '-50vw',
-  //         backgroundRepeat: 'no-repeat',
-  //         backgroundSize: 'cover',
-  //       }}
-  //     >
-  //       <div
-  //         style={{
-  //           padding: '10em',
-  //           display: 'flex',
-  //           flexDirection: 'column',
-  //           alignItems: 'center',
-  //         }}
-  //       >
-  //         <h1>AI System Inventory</h1>
-  //         <div
-  //           style={{
-  //             width: '55%',
-  //           }}
-  //         >
-  //           You're one stop shop to see all AI System Inventory in one spot.
-  //         </div>
-  //       </div>
-  //     </div>
-  //   );
-  // }
+  function SystemInventoryBackground() {
+    return (
+      // <div
+      //   style={{
+      //     backgroundImage: `url(${backgroundImage})`,
+      //     width: '99.6vw',
+      //     height: '35vh',
+      //     position: 'relative',
+      //     left: '50%',
+      //     right: '50%',
+      //     top: '-100px',
+      //     marginLeft: '-50vw',
+      //     marginRight: '-50vw',
+      //     backgroundRepeat: 'no-repeat',
+      //     backgroundSize: 'cover',
+      //   }}
+      // >
+      <div
+        style={{
+          padding: '10em',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <h1>AI System Inventory</h1>
+        <div>
+          You're one stop shop to see all AI System Inventory in one spot.
+        </div>
+      </div>
+      // </div>
+    );
+  }
 
   return (
     <div>
-      {/* <SystemInventoryBackground /> */}
+      <SystemInventoryBackground />
       <Login />
-      <Container>
-        <SystemInventoryGrid></SystemInventoryGrid>
-      </Container>
+
+      <SystemInventoryGrid></SystemInventoryGrid>
     </div>
   );
 }
